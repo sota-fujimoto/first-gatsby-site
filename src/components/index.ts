04c89140d4ffb8235024223ae2dc245db56fc974
Souta Fujimoto <souta.fujimoto@wealth-park.com>
2022-01-24 22:48:40 +0900

    import graphql from gatsby

---
+++ src/components/index.ts
@@ -0,0 +1,25 @@
+import { graphql } from "gatsby";
+
+//...
+export const pageQuery = graphql`
+  query BlogIndex {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+`
